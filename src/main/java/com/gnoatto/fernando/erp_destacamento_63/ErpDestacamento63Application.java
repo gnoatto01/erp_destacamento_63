dbc2649fbf9b648ec4566ffba7af2d100f9b13ff
Fernando Augusto Gnoatto <fernn65@hotmail.com>
2024-01-07 00:24:11 -0300

    commit inicial

---
+++ src/main/java/com/gnoatto/fernando/erp_destacamento_63/ErpDestacamento63Application.java
@@ -0,0 +1,13 @@
+package com.gnoatto.fernando.erp_destacamento_63;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpDestacamento63Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpDestacamento63Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gnoatto/fernando/erp_destacamento_63/ErpDestacamento63ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnoatto.fernando.erp_destacamento_63;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpDestacamento63ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
